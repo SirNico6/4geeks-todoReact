1853fb1c12c3b14a205c0a7b04dc3e9094cf8922
Nicolas Arevalo <gabrielnicolas512@outlook.com>
2024-10-21 21:13:48 -0300

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,52 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
+import "../../styles/index.css"
 
 //create your first component
 const Home = () => {
+	const [taskList, setTaskList] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+
+	const addTask = (e) => {
+		if (e.key === 'Enter' && inputValue.trim() !== "") {
+			setTaskList([...taskList, inputValue]); // Agregar la nueva tarea al array
+			setInputValue(""); // Limpiar el input
+		}
+	};
+
+	const deleteTask = (index) => {
+		// Elimina la tarea del taskList
+		setTaskList(taskList.filter((_, i) => i !== index));
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-column justify-content-center align-items-center vh-100">
+			<h3 className="title">TODOS</h3>
+			<input
+				type="text"
+				onChange={e => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyDown={addTask}
+				className="form-control mb-3 input"
+				placeholder="What needs to be done?"
+			/>
+			{taskList.length > 0 ? (
+				<>
+					<ul className="list-group w-25 taskList">
+						{taskList.map((task, index) => (
+							<li className="list-group-item d-flex justify-content-between align-items-center" key={index} style={{ border: 'none', borderBottom: '1px solid #e0e0e0' }}>
+								<span>{task}</span>
+								<button type="button" className="btn-close" aria-label="Close" onClick={() => deleteTask(index)}></button>
+							</li>
+						))}
+					</ul>
+					<small className="text-muted">
+						{taskList.length} item{taskList.length !== 1 ? 's' : ''} left
+					</small>
+				</>
+			) : (
+				<p className="text-muted">No tasks, add a task</p>
+			)}
 		</div>
 	);
 };
